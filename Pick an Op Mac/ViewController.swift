cf0cd67a091ca6d21129bd23aacc6c068c7ec6c6
Justin Krysinski <justinkrysinski@justins-imac.local>
2018-05-31 20:39:21 -0500

    Initial Commit

---
+++ Pick an Op Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pick an Op Mac
+//
+//  Created by Justin Krysinski on 5/31/18.
+//  Copyright © 2018 Justin Krysinski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
